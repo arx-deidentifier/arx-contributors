6718bf4f3b31d424ca2fa61bea9cdf88ae108b5c
Fabian Prasser <mail@fabian-prasser.de>
2016-02-16 17:58:18 +0100

    Update

--- analyze.sh
+++ analyze.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 git clone https://github.com/ejwa/gitinspector.git
 git clone https://github.com/arx-deidentifier/arx.git
-git config diff.renameLimit 10000
+git config merge.renameLimit 999999
+git config --unset merge.renameLimit
+git config diff.renameLimit 999999
+git config --unset diff.renameLimit
 ./gitinspector/gitinspector.py --format=htmlembedded --file-types=java --hard -x example -x test -x gui -w arx/ > statistics-main.html
 ./gitinspector/gitinspector.py --format=htmlembedded --file-types=java --hard -x example -x test -x main -w arx/ > statistics-gui.html
 ./gitinspector/gitinspector.py --format=htmlembedded --file-types=java --hard -x main -x test -x gui -w arx/ > statistics-example.html
