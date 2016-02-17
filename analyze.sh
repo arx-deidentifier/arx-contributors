85e5dae3e773df2f1b74dd5896765d8425bf46f6
Fabian Prasser <mail@fabian-prasser.de>
2016-02-17 14:42:51 +0100

    Update code

--- analyze.sh
+++ analyze.sh
@@ -15,10 +15,10 @@ git config --global diff.renameLimit 999999
 ./gitinspector/gitinspector.py --format=text --since=2013-05-10 --file-types=java --hard -x main -x test -x gui -w arx/ > statistics-example.txt
 ./gitinspector/gitinspector.py --format=text --since=2013-05-10 --file-types=java --hard -x example -x main -x gui -w arx/ > statistics-test.txt
 
-git fame --repository=arx --since=2013-05-10 --whitespace --exclude=logo,lib,doc,data,src/example,src/gui,src/test > statistics-main.csv
-git fame --repository=arx --since=2013-05-10 --whitespace --exclude=logo,lib,doc,data,src/example,src/main,src/test > statistics-gui.csv
-git fame --repository=arx --since=2013-05-10 --whitespace --exclude=logo,lib,doc,data,src/example,src/gui,src/main > statistics-test.csv
-git fame --repository=arx --since=2013-05-10 --whitespace --exclude=logo,lib,doc,data,src/main,src/gui,src/test > statistics-example.csv
+git fame --progressbar=0 --repository=arx --since=2013-05-10 --whitespace --exclude=logo,lib,doc,data,src/example,src/gui,src/test > statistics-main.csv
+git fame --progressbar=0 --repository=arx --since=2013-05-10 --whitespace --exclude=logo,lib,doc,data,src/example,src/main,src/test > statistics-gui.csv
+git fame --progressbar=0 --repository=arx --since=2013-05-10 --whitespace --exclude=logo,lib,doc,data,src/example,src/gui,src/main > statistics-test.csv
+git fame --progressbar=0 --repository=arx --since=2013-05-10 --whitespace --exclude=logo,lib,doc,data,src/main,src/gui,src/test > statistics-example.csv
 
 gem uninstall --all --executables git_fame
 rm -rf ./arx
