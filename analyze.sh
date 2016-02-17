497bfc40343c575a3c2bbb8f06ef7cb82638e2b9
Fabian Prasser <mail@fabian-prasser.de>
2016-02-17 11:49:28 +0100

    Do not embedd jquery

--- analyze.sh
+++ analyze.sh
@@ -5,9 +5,9 @@ git config merge.renameLimit 999999
 git config --unset merge.renameLimit
 git config diff.renameLimit 999999
 git config --unset diff.renameLimit
-./gitinspector/gitinspector.py --format=htmlembedded --file-types=java --hard -x example -x test -x gui -w arx/ > statistics-main.html
-./gitinspector/gitinspector.py --format=htmlembedded --file-types=java --hard -x example -x test -x main -w arx/ > statistics-gui.html
-./gitinspector/gitinspector.py --format=htmlembedded --file-types=java --hard -x main -x test -x gui -w arx/ > statistics-example.html
-./gitinspector/gitinspector.py --format=htmlembedded --file-types=java --hard -x example -x main -x gui -w arx/ > statistics-test.html
+./gitinspector/gitinspector.py --format=html --file-types=java --hard -x example -x test -x gui -w arx/ > statistics-main.html
+./gitinspector/gitinspector.py --format=html --file-types=java --hard -x example -x test -x main -w arx/ > statistics-gui.html
+./gitinspector/gitinspector.py --format=html --file-types=java --hard -x main -x test -x gui -w arx/ > statistics-example.html
+./gitinspector/gitinspector.py --format=html --file-types=java --hard -x example -x main -x gui -w arx/ > statistics-test.html
 rm -rf ./arx
 rm -rf ./gitinspector
