8781dea8fd2d7c3106780ee7a72d437c2e244554
Fabian Prasser <mail@fabian-prasser.de>
2016-02-17 13:11:45 +0100

    Update

--- analyze.sh
+++ analyze.sh
@@ -3,10 +3,10 @@ git clone https://github.com/ejwa/gitinspector.git
 git clone https://github.com/arx-deidentifier/arx.git
 git config --global merge.renameLimit 999999
 git config --global diff.renameLimit 999999
-./gitinspector/gitinspector.py --format=html --since=2013-05-10 --grading=true --metrics=true --file-types=java --hard -x example -x test -x gui -w arx/ > statistics-main.html
-./gitinspector/gitinspector.py --format=html --since=2013-05-10 --grading=true --metrics=true --file-types=java --hard -x example -x test -x main -w arx/ > statistics-gui.html
-./gitinspector/gitinspector.py --format=html --since=2013-05-10 --grading=true --metrics=true --file-types=java --hard -x main -x test -x gui -w arx/ > statistics-example.html
-./gitinspector/gitinspector.py --format=html --since=2013-05-10 --grading=true --metrics=true --file-types=java --hard -x example -x main -x gui -w arx/ > statistics-test.html
+./gitinspector/gitinspector.py --format=html --since=2013-05-10 --file-types=java --hard -x example -x test -x gui -w arx/ > statistics-main.html
+./gitinspector/gitinspector.py --format=html --since=2013-05-10 --file-types=java --hard -x example -x test -x main -w arx/ > statistics-gui.html
+./gitinspector/gitinspector.py --format=html --since=2013-05-10 --file-types=java --hard -x main -x test -x gui -w arx/ > statistics-example.html
+./gitinspector/gitinspector.py --format=html --since=2013-05-10 --file-types=java --hard -x example -x main -x gui -w arx/ > statistics-test.html
 rm -rf ./arx
 rm -rf ./gitinspector
 git add statistics-main.html
