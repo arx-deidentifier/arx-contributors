9f7bf883e5fb063f60329292a0731ffe7f2c4c40
Fabian Prasser <mail@fabian-prasser.de>
2016-02-17 14:41:22 +0100

    Update code

--- analyze.sh
+++ analyze.sh
@@ -10,17 +10,17 @@ cd ..
 git config --global merge.renameLimit 999999
 git config --global diff.renameLimit 999999
 
-./gitinspector/gitinspector.py --format=html --since=2013-05-10 --file-types=java --hard -x example -x test -x gui -w arx/ > statistics-main.html
-./gitinspector/gitinspector.py --format=html --since=2013-05-10 --file-types=java --hard -x example -x test -x main -w arx/ > statistics-gui.html
-./gitinspector/gitinspector.py --format=html --since=2013-05-10 --file-types=java --hard -x main -x test -x gui -w arx/ > statistics-example.html
-./gitinspector/gitinspector.py --format=html --since=2013-05-10 --file-types=java --hard -x example -x main -x gui -w arx/ > statistics-test.html
+./gitinspector/gitinspector.py --format=text --since=2013-05-10 --file-types=java --hard -x example -x test -x gui -w arx/ > statistics-main.txt
+./gitinspector/gitinspector.py --format=text --since=2013-05-10 --file-types=java --hard -x example -x test -x main -w arx/ > statistics-gui.txt
+./gitinspector/gitinspector.py --format=text --since=2013-05-10 --file-types=java --hard -x main -x test -x gui -w arx/ > statistics-example.txt
+./gitinspector/gitinspector.py --format=text --since=2013-05-10 --file-types=java --hard -x example -x main -x gui -w arx/ > statistics-test.txt
 
 git fame --repository=arx --since=2013-05-10 --whitespace --exclude=logo,lib,doc,data,src/example,src/gui,src/test > statistics-main.csv
 git fame --repository=arx --since=2013-05-10 --whitespace --exclude=logo,lib,doc,data,src/example,src/main,src/test > statistics-gui.csv
 git fame --repository=arx --since=2013-05-10 --whitespace --exclude=logo,lib,doc,data,src/example,src/gui,src/main > statistics-test.csv
 git fame --repository=arx --since=2013-05-10 --whitespace --exclude=logo,lib,doc,data,src/main,src/gui,src/test > statistics-example.csv
 
-gem uninstall --all git_fame
+gem uninstall --all --executables git_fame
 rm -rf ./arx
 rm -rf ./gitinspector
 
